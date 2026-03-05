37c1b5831729181419679254790035ed1c4860b7
Tealize <58103246+Tealize-Write@users.noreply.github.com>
2026-03-05 19:21:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,5 @@
 const GAS_URL   = "https://script.google.com/macros/s/AKfycbw9_PkjEU5h46a77SILoy51G1cicDcNQrkHPdgSemyxEaftPzRQzZYrvmcPXCEuKoWY4w/exec";
 const GAS_TOKEN = "zE6mJ5rDGeD7PpJ5dH3aYbF7dcE5dHuK1sH";
 
-/* ════════════════════════════════
-   AXES
-════════════════════════════════ */
-
-const AXES = ["dom","sub","control","manip","chaos","devotion"];
-
-
 const ROMAN=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII'];
 
